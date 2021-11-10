ed06de03800301bbd56bbd686fc4870a86fb1496
Javier Ruiz de Valdivia <javiruiz314@gmail.com>
2021-11-10 18:54:11 +0100

    Adds header component

---
+++ src/modules/core/header/Header.tsx
@@ -0,0 +1,13 @@
+import './Header.css';
+import React from "react";
+
+const Header = () => {
+
+	return (
+		<header className="header" >
+				<h2 className='header-text'>Shape drawer</h2>
+		</header>
+	);
+};
+
+export default Header;
