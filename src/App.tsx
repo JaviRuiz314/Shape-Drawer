dda06c67a30274607702ed3ede1cfb1068b834b9
Javier Ruiz de Valdivia <javiruiz314@gmail.com>
2021-11-10 19:07:26 +0100

    Adds app height

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './modules/core/header/Header';
+import Main from './modules/core/main/Main';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Main />
     </div>
   );
 }
