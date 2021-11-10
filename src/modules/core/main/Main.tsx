6691f0cd2a774632eacd94dc2c281b73008a5efc
Javier Ruiz de Valdivia <javiruiz314@gmail.com>
2021-11-10 20:46:22 +0100

    Adds canvas component to main component

--- src/modules/core/main/Main.tsx
+++ src/modules/core/main/Main.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Canvas from '../../ui/canvas/Canvas';
 import './Main.css';
 
 const Main = () => {
 	return (
 		<div className='main'>
+			<div className='image-placeholder'>
+				<Canvas height={750} width={800}/>
+			</div>
 
 		</div>
 	)
