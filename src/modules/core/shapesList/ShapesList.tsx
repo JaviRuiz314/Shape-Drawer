1f37cff8e46ad8cbd88975a14221376167497dc2
Javier Ruiz de Valdivia <javiruiz314@gmail.com>
2021-11-13 17:46:35 +0100

    Adds disabled feature when user types shapes name

--- src/modules/core/shapesList/ShapesList.tsx
+++ src/modules/core/shapesList/ShapesList.tsx
@@ -7,23 +7,32 @@ import './ShapesList.css'
 const ShapesList = ({ shapeList }: IShapeListProps) => {
 
 	const namePolygon = (event: React.FocusEvent<HTMLInputElement>) => {
-		shapeList[parseInt(event.target.id, 10)].name = event.target.value;
-	} 
+		// If the name of the shape can't never be edited we can simply access the last shape of the
+		// array but I am implementing a forEach method in case we would want to add an edit name feature
+		// in the future.
+		shapeList.forEach(shape => (
+			shape.name = shape.id === parseInt(event.target.id, 10) ?
+				event.target.value : shape.name
+		));
+
+		const input: any = document.getElementById(event.target.id);
+		if(input && input.disabled) input.disabled = true;
+	}
 
 	const renderListOfShapes = (): any => {
-		return(
+		return (
 			<ul>
-			{shapeList.map((shape) => {
-				return(
-				<li key={shape.id}> <Input id={shape.id.toString()} type='text' placeholder="Enter a new name..."  onBlur={namePolygon}/></li>)
-			})}
-		</ul>
+				{shapeList.map((shape) => {
+					return (
+						<li key={shape.id}> <Input id={shape.id.toString()} type='text' placeholder="Enter a new name..." onBlur={namePolygon} /></li>)
+				})}
+			</ul>
 		);
 	}
 
 	return (
 		<div className="shape-list">
-			{ renderListOfShapes() }
+			{renderListOfShapes()}
 		</div>
 	);
 }
